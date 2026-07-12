bb8d0ed545dc85880a42556085bbb3b6052b2fc2
rd2368351-design <rd2368351@gmail.com>
2026-07-12 15:12:23 +0530

    Update lib.rs

--- crates/types/src/lib.rs
+++ crates/types/src/lib.rs
@@ -21,6 +21,7 @@ pub mod queue_id;
 pub mod attachment_id;
 pub mod folder_id;
 pub mod device_id;
+pub mod api_key_id;
 
 pub use error::Error;
 pub use id::Id;
@@ -39,3 +40,4 @@ pub use queue_id::QueueId;
 pub use attachment_id::AttachmentId;
 pub use folder_id::FolderId;
 pub use device_id::DeviceId;
+pub use api_key_id::ApiKeyId;
